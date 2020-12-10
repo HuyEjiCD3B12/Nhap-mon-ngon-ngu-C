169fa8ac7c3801b1d7c01eaadf3a9d43454a9d86
HuyEjiCD3B12 <75779345+HuyEjiCD3B12@users.noreply.github.com>
2020-12-10 16:59:02 +0700

    Create thanhtien.c

---
+++ Bài tập 3/thanhtien.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+	int d , s;
+	printf("dong gia d =");
+	scanf("%d", &d);
+	printf("soluong s =");
+	scanf(%d, &s );
+	printf("d * s =%d", d * s);
+	if(s >= 5);
+	(s * d)*0,75 = thanh tien;
+	
+}
