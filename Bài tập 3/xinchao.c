a24050b2443df8e233b15ff3f995d1aa52299ddb
HuyEjiCD3B12 <75779345+HuyEjiCD3B12@users.noreply.github.com>
2020-12-24 14:40:31 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main (){
+	int number;
+	printf("\t hien thi 10 dong\n ");
+	for(number =1; number <=9; number ++)
+	printf("\n\t\t xin chao");
+	printf("\n\t\t xin chao.\n");
+}
